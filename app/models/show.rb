37ca3b217f0e5f201fcbc8e220ce0659d66501c5
Patrick Brennan <pbrennanmusic@gmail.com>
2021-06-07 01:36:03 -0400

    commit tree 8f1ad29cbba4814c715e27f21356b059781f13ec

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+
+    self.actors.map {|a| a.full_name }
+  end
+
+  def build_network(hash)
+    network = Network.create(hash).tap {|n| n.shows << self }
+  end
 end
